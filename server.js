3b7b7ae80e61898333b53c9349d790f655974209
Ronald Nelson <nelsonri21@gmail.com>
2015-12-28 07:46:43 -0500

    Added disconnect event and message

--- server.js
+++ server.js
@@ -8,12 +8,23 @@ var now = moment();
 
 app.use(express.static(__dirname + '/public'));
 
-var clientInfo = {
-
-};
+var clientInfo = {};
 
 io.on('connection', function (socket) {
 	console.log('User Connected via Socket.IO!');
+
+	socket.on('disconnect', function () {
+		var userData = clientInfo[socket.id];
+		if (typeof userData !== 'undefined') {
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left!',
+				timeStamp: now.valueOf()
+			});
+			delete userData;
+		}
+	});
 
 	socket.on('joinRoom', function (req) {
 		clientInfo[socket.id] = req;
